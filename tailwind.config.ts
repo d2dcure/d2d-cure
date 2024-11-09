0e8b93049536d09cb1bea3b4d04c03dd5da2cd7b
Mohnish <mohnishgopi@gmail.com>
2024-11-09 11:52:04 -0800

    LOGO CAROUSEL

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,12 +16,17 @@ const config: Config = {
   theme: {
     extend: {
       animation: {
-        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+        'infinite-scroll': 'infinite-scroll 60s linear infinite',
+        'infinite-scroll-reverse': 'infinite-scroll-reverse 60s linear infinite',
       },
       keyframes: {
         'infinite-scroll': {
           from: { transform: 'translateX(0)' },
           to: { transform: 'translateX(-100%)' },
+        },
+        'infinite-scroll-reverse': {
+          from: { transform: 'translateX(-100%)' },
+          to: { transform: 'translateX(0)' },
         }
       }
     },
