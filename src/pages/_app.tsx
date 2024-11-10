b6c9538139c12beb6fac73b1bb0060be595ce731
Mohnish <mohnishgopi@gmail.com>
2024-11-10 11:14:48 -0800

    add logged in notifications, and team

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,8 @@ import { UserProvider } from '@/components/UserProvider';
 import AuthStateListener from '@/components/AuthStateListener';
 import dynamic from 'next/dynamic';
 import "../app/globals.css"; 
-
+import LoginSuccessNotification from '@/components/LoginSuccessNotification';
+import LogoutSuccessNotification from '@/components/LogoutSuccessNotification';
 
 const FlowbiteInit = dynamic(
   () => import('@/components/FlowbiteInit'),
@@ -12,10 +13,11 @@ const FlowbiteInit = dynamic(
 );
 
 function MyApp({ Component, pageProps }: AppProps) {
-
   return (
     <UserProvider>
       <AuthStateListener />
+      <LoginSuccessNotification />
+      <LogoutSuccessNotification />
       <Component {...pageProps} />
       <FlowbiteInit />
     </UserProvider>
