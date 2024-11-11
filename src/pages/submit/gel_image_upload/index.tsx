2c3da7a87392c32b2afffbd8fbe1451ad32ef995
Hussain Ali <HussainAli22422@gmail.com>
2024-11-10 22:02:15 -0800

    okay this should fix the vercel error

--- src/pages/submit/gel_image_upload/index.tsx
+++ src/pages/submit/gel_image_upload/index.tsx
@@ -1,7 +1,31 @@
+// TODO: rename file before uploading to S3, something like [variant]-[institution]-[username].png 
+
+
 import React, { useCallback } from "react";
-import { uploadFileToS3 } from "./s3Utils"; // We will create this function next
+import s3 from "../../../../s3config"; 
+import { useUser } from '@/components/UserProvider'; 
+
+const BUCKET_NAME = "d2dcurebucket"; 
 
 const DragAndDropUpload: React.FC = () => {
+  const uploadFileToS3 = (file: File) => {
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: `gel-images/${file.name}`,
+      Body: file,
+    };
+
+    s3.upload(params, (err: any, data: any) => {
+      if (err) {
+        console.error("Error uploading file:", err);
+        alert("Error uploading file.");
+      } else {
+        console.log("File uploaded successfully:", data.Location);
+        alert(`File uploaded successfully: ${data.Location}`);
+      }
+    });
+  };
+
   const onFileChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
 
