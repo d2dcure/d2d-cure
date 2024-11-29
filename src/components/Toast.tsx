d7eacc0a464ad9d1caa3c0bca86f537d5f7c5af9
hussiiii <130942545+hussiiii@users.noreply.github.com>
2024-11-28 20:38:27 -0800

    Update Toast.tsx again

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -76,7 +76,7 @@ const Toast = ({
 
   if (!show) return null;
 
-//  const { icon } = getIconAndHelper();  why is this here? - Hussain
+  const { icon } = getIcon();  why is this here? - Hussain
 
   return (
     <div className={`
