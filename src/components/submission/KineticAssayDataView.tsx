f189d4579e8903593476b08ba4970aea2b720d7c
Mohnish <mohnishgopi@gmail.com>
2024-12-01 22:42:37 -0800

    new

--- src/components/submission/KineticAssayDataView.tsx
+++ src/components/submission/KineticAssayDataView.tsx
@@ -778,7 +778,7 @@ const KineticAssayDataView: React.FC<KineticAssayDataViewProps> = ({
         </div>
 
         {/* Updated responsive card */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
           <Card className="min-h-[200px] w-full">
             <CardBody className="pt-8 font-light overflow-hidden">
               <div className="flex items-start space-x-4">
