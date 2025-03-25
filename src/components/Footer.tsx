f857cc6784c3d6def8ce77cb21710c267e0573c5
hussiiii <130942545+hussiiii@users.noreply.github.com>
2025-03-24 20:58:14 -0700

    fix: remove duplicate copyright notices from footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer>
+      <p>© {currentYear} Your Company Name. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
