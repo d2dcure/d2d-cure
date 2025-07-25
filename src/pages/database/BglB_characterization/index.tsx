203bbf90520f61e5388b9bd48e51117eada4e30b
Labonte <jlabonte@ndm.edu>
2025-07-25 15:29:45 -0400

    last try

--- src/pages/database/BglB_characterization/index.tsx
+++ src/pages/database/BglB_characterization/index.tsx
@@ -1220,9 +1220,9 @@ const DataPage = () => {
                             size="md"
                             onClick={downloadCSV}
                           >
-                            Download filtered/displayed data<br />
-			    <br /><small>({displayData.length} records)</small><br />
-			    <br />as comma-delimited file
+				Download filtered/displayed data<br></br>br>
+				<small>({displayData.length} records)</small></br>br>
+				as comma-delimited file
                           </Button>
                         </div>
 
