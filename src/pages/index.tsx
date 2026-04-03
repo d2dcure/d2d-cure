4b14b6ec861fabc00a54d734e2fd38bfe8011d18
Labonte <jlabonte@ndm.edu>
2026-04-02 23:52:44 -0400

    proofreading; welcome message to logged-in user; fixing broken button; clearer button label; proper alt text for some images; complete up to first image of OG gang

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,71 +34,88 @@ export default function Home() {
 	return (
 		<div>
 			<NavBar />
+			{/* Greet user, if logged in. */}
+			{user && (
+				<>
+					<div className="max-w-2xl text-center mx-auto">
+						<p>Welcome, {user.given_name}!</p>
+					</div>
+				</>
+			)}
 			{/* Container for content with responsive padding */}
-<section
-	className="pt-14 lg:pt-14 bg-[url('/resources/images/ng.png')] bg-center bg-fill bg-no-repeat"
->
-	<div className="max-w-2xl text-center mx-auto">
-		<div className="flex pb-3 flex-row justify-center items-center pb-1">
-			<img src="/resources/images/D2D_Logo.svg" draggable="false" className="h-12 sm:h-16 lg:h-20 select-none pr-2 lg:pr-3" alt="logo" />
-			<h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold font-poppins" style={{ color: '#3C99AC', opacity: 0.38 }}>CURE</h1>
-		</div>
-		<p className="text-base sm:text-lg lg:text-xl font-light text-[#518C98]">
-			Unpacking protein structure-to-function relationships
-		</p>
-		<p className="text-base sm:text-lg lg:text-xl mb-5 font-light text-[#518C98]">
-			through large, high-resolution, quantitative datasets.
-		</p>
-		<div className="flex space-x-4 pt-2 pb-10 justify-center">
-			<Button
-				className="bg-[#06B7DB] text-white rounded-lg px-6 py-2 text-lg transition-all duration-300 hover:scale-105"
-				size="md"
-				onClick={() => router.push(user?.user_name ? '/dashboard' : '/login')}
+			<section
+				className="pt-14 lg:pt-14 bg-[url('/resources/images/ng.png')] bg-center bg-fill bg-no-repeat"
 			>
-				{user?.user_name ? "Dashboard" : "Get Started"}
-			</Button>
-			<Button
-				className="bg-transparent text-[#06B7DB] border border-[#06B7DB] rounded-lg px-6 py-2 text-lg transition-all duration-300 hover:scale-105"
-				size="md"
-				color="primary"
-				onClick={() => router.push('/about')}
-			>
-				Learn More
-			</Button>
-		</div>
-	</div>
+				<div className="max-w-2xl text-center mx-auto">
+					<div className="flex pb-3 flex-row justify-center items-center pb-1">
+						<img
+							src="/resources/images/D2D_Logo.svg" draggable="false"
+							className="h-12 sm:h-16 lg:h-20 select-none pr-2 lg:pr-3"
+							alt="D2D"
+						/>
+						<h1
+							className="text-4xl sm:text-5xl lg:text-6xl font-bold font-poppins"
+							style={{ color: '#3C99AC', opacity: 0.38 }}>Design2Data</h1>
+					</div>
+					<p className="text-base sm:text-lg lg:text-xl font-light text-[#518C98]">
+						Unpacking protein structure-to-function relationships<br />
+						through large, high-resolution, quantitative datasets
+					</p>
 
-	<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-10 relative text-center">
-		<div className="flex justify-center">
-			<img
-				src="/resources/images/thumb.png"
-				alt="Dashboard image" 
-				className="rounded-t-3xl w-full max-w-[1000px] select-none h-auto object-cover" 
-				draggable="false"
-			/>
-		</div>
-	</div>
-</section>
-																						
+					<div className="flex space-x-4 pt-2 pb-10 justify-center">
+						<Button
+							className="bg-[#06B7DB] text-white rounded-lg px-6 py-2 text-lg transition-all duration-300 hover:scale-105"
+							size="md"
+							onClick={() => router.push(user?.user_name ? '/dashboard' : '/login')}
+						>
+							{user?.user_name ? "Dashboard" : "Sign-In / Create Account"}
+						</Button>
+						<Button
+							className="bg-transparent text-[#06B7DB] border border-[#06B7DB] rounded-lg px-6 py-2 text-lg transition-all duration-300 hover:scale-105"
+							size="md"
+							color="primary"
+							onClick={() => router.push('/about')}
+						>
+							Learn More
+						</Button>
+					</div>
+				</div>
 
-
-
+				<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-10 relative text-center">
+					<div className="flex justify-center">
+						<img
+							src="/resources/images/thumb.png"
+							alt="Dashboard image" 
+							className="rounded-t-3xl w-full max-w-[1000px] select-none h-auto object-cover" 
+							draggable="false"
+						/>
+					</div>
+				</div>
+			</section>
 
 			<div className="mx-4 sm:mx-8 lg:mx-24 py-20 bg-white">
-
 				{/* Mission Section */}
 				<div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-40 mt-10">
 					<div className="lg:pt-1">
-						<p className="text-lg font-regular text-gray-500" style={{ color: '#06B7DB' }}>
+						<h2 className="text-lg font-regular text-gray-500" style={{ color: '#06B7DB' }}>
 							OUR MISSION
-						</p>
-						<h1 className="text-2xl lg:text-4xl mb-4 font-inter dark:text-white">Innovative Protein Engineering</h1>
+						</h2>
+						<h3 className="text-2xl lg:text-4xl mb-4 font-inter dark:text-white">
+							Innovative Protein Engineering
+						</h3>
 						<p className="mb-6 pb-6 text-gray-500 text-lg dark:text-gray-200">
-						The Design-to-Data workflow was developed in the Siegel Lab with the central research of improving the current predictive limitations of protein modeling software by functionally characterizing single amino acid mutants in a robust model system. This workflow is undergraduate-friendly, and students have an opportunity to practice protein design, kunkel mutagenesis, and enzyme characterization assays. The workflow is intuitively organized through engineering’s conceptual progression of design-build-test. 
+							The Design2Data workflow was developed in the Siegel Lab
+							with the central research goal of improving the current predictive limitations of protein-modeling software
+							by functionally characterizing single-amino-acid enzyme variants in a robust model system.
+							This workflow is undergraduate-friendly,
+							and students have an opportunity to practice protein design,
+							mutagenesis, and enzyme-characterization assays.
+							The workflow is intuitively organized through engineering’s conceptual progression of
+							design&ndash;build&ndash;test.
 						</p>
 						<Button
 							variant="bordered"
-							onPress={() => window.location.href = '/login'}
+							onPress={() => window.location.href = '/about'}
 							className="w-[250px] h-[40px] font-semibold border-[#06B7DB] border-2 text-[#06B7DB] transition-all duration-300 hover:bg-[#06B7DB] hover:text-white hover:shadow-xl hover:scale-105"
 						>
 							Learn More
@@ -109,7 +126,7 @@ export default function Home() {
 							src="/resources/images/Homepage.png" 
 							draggable="false" 
 							className="select-none rounded-lg object-cover max-w-full" 
-							alt="mockup" 
+							alt="An image of the original cohort of professors in the D@D Network." 
 						/>
 					</div>
 				</div>
