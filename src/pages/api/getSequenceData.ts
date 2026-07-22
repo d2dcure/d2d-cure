6d60629483e6219dacd6052da6703aaa59b84544
Labonte <jlabonte@ndm.edu>
2026-07-22 00:45:01 -0400

    ensuring new API work in other files

--- src/pages/api/getSequenceData.ts
+++ src/pages/api/getSequenceData.ts
@@ -9,9 +9,7 @@ export default async function handler(req: any, res: any) {
 		const client = getClient(enzyme);
         try {
             const data = await client.sequence.findMany({
-                orderBy: {
-                    id: 'asc'
-                }
+                orderBy: {id: 'asc'}
             });
             res.status(200).json(data);
         } catch (error) {
