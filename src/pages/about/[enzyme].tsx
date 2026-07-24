25c9d7b4b8fee3b96c518183c83bf638b74271c1
Labonte <jlabonte@ndm.edu>
2026-07-24 16:44:39 -0400

    big improvements to styling, but more to do; removed cards

--- src/pages/about/[enzyme].tsx
+++ src/pages/about/[enzyme].tsx
@@ -42,6 +42,7 @@ const AboutEnzymePAge = () => {
 	const [sequenceData, setSequenceData] = useState<SequenceData[]>([]);
 	const [characterizationData, setCharacterizationData] = useState<any[]>([]);
 
+	// Fetch data whenever selected enzyme changes.
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
@@ -123,297 +124,272 @@ const AboutEnzymePAge = () => {
 	return (
 		<>
 			<NavBar />
+
+			{/* Main Body: TODO: Add <main>? */}
 			<div className="px-6 md:px-12 lg:px-24 py-8 lg:py-10 bg-white">
+				{/* Breadcrumbs: TODO: Autogenerate, standardize, and import. */}
 				<div className="max-w-7x1 mx-auto">
 					<Breadcrumbs>
 						<BreadcrumbItem href="/">Home</BreadcrumbItem>
 						<BreadcrumbItem href="/about">About</BreadcrumbItem>
 						<BreadcrumbItem>{abbr}</BreadcrumbItem>
 					</Breadcrumbs>
-		</div>
-		<div className="max-w-7x1 mx-auto pt-8 mb-8">
-						<h2 className="mb-2 text-3xl md:text-4xl lg:text-5xl font-inter dark:text-white">
-							About {full_name} ({abbr})
-						</h2>
-			{coming_soon && (
-				<p>(Coming soon!)</p>
-			)}
-			{!coming_soon && (
-				<p>(Added to D2D Network in {year})</p>
-			)}
-
-
-			{/* Info Section */}
-				<div className="inline-grid grid-cols-2 grid-rows-1 gap-4">
-					<div>
-						<div className="space-y-2 mb-8">
-				<h3>Identifiers</h3>
-							<div className="flex items-center gap-3">
-								<span className="font-semibold w-44 text-sm text-gray-600">
-					Species:
-				</span>
-								<Link
-					href={species_link}
-					className="text-sm text-blue-500"
-					underline="hover"
-					isExternal
-					showAnchorIcon
-				>
-					<i>{species}</i>
-				</Link>
-							</div>
-							<div className="flex items-center gap-3">
-								<span className="font-semibold w-44 text-sm text-gray-600">
-					<abbr title="Enzyme Commission">EC</abbr> Number:
-				</span>
-								<Link
-					href={EC_link}
-					className="text-sm text-blue-500"
-					underline="hover"
-					isExternal
-					showAnchorIcon
-				>
-					{EC_number}
-				</Link>
-							</div>
-							<div className="flex items-center gap-3">
-								<span className="font-semibold w-44 text-sm text-gray-600">
-					<abbr title="Universal Protein Resource">UniProt</abbr> Number:
-				</span>
-								<Link
-					href={UniProt_link}
-					className="text-sm text-blue-500"
-					underline="hover"
-					isExternal
-					showAnchorIcon
-				>
-					{UniProt_number}
-				</Link>
-							</div>
-				{PDB_entries && (
-							<div className="flex items-center gap-3">
-								<span className="font-semibold w-44 text-sm text-gray-600">
-					<abbr title="Protein Data Bank">PDB</abbr> Entries:
-				</span>
-								<div className="flex flex-wrap gap-2 text-sm">
-					{pdbs.map((pdb) => (
-						<Link
-							key={pdb}
-							href={PDB_link(pdb)}
-							className="text-sm text-blue-500"
-							underline="hover"
-							isExternal
-							showAnchorIcon
-						>
-							{pdb}
-						</Link>
-					))}
-								</div>
-							</div>
-				)}
-				{AlphaFold_entries && (
-							<div className="flex items-center gap-3">
-								<span className="font-semibold w-44 text-sm text-gray-600">
-					AlphaFold Entries:
-				</span>
-								<div className="flex flex-wrap gap-2 text-sm">
-					{models.map((model) => (
-						<Link
-							key={model}
-							href={AlphaFold_link(model)}
-							className="text-sm text-blue-500"
-							underline="hover"
-							isExternal
-							showAnchorIcon
-						>
-							AF-{model}
-						</Link>
-					))}
-								</div>
-							</div>
-				)}
-			</div>
-
-			<div className="space-y-2">
-				<h3>Properties</h3>
-							<div className="flex items-center gap-3">
-								<span className="font-semibold w-44 text-sm text-gray-600">
-					Subunits (in active form):
-				</span>
-								<span className="text-sm text-gray-600">{subunits}</span>
-							</div>
-							<div className="flex items-center gap-3">
-								<span className="font-semibold w-44 text-sm text-gray-600">
-					Molar Mass (<i>M</i>, per subunit):
-				</span>
-								<span className="text-sm text-gray-600">
-					{molar_mass}{' '}
-					<abbr title="daltons (g/mol)">Da</abbr>
-				</span>
-							</div>
-							<div className="flex items-center gap-3">
-								<span className="font-semibold w-44 text-sm text-gray-600">
-					Extinction Coefficient (ε):
-				</span>
-								<span className="text-sm text-gray-600">
-					{ext_coefficient}{' '}
-					<abbr title="inverse molar per centimeter">
-						ᴍ<sup>&minus;1</sup> cm<sup>&minus;1</sup>
-					</abbr>
-				</span>
-							</div>
-						</div>
-					</div>
-
-			{/* Image */}
-					<div>
-						<Image 
-				isZoomed
-							src={pretty_image}
-				title={pretty_image_title}
-							alt="3D Structure"
-
-							className="w-[280px] sm:w-[320px] lg:w-[350px] max-w-full"
-						/>
-					</div>
 				</div>
 
-				<div className="mt-12 space-y-6 max-w-3xl">
-					<p className="text-gray-600 text-justify">
-						{pathway_desc}
-					</p>
-			<Accordion isCompact variant="shadow">
-			<AccordionItem
-				key="1"
-				title="Biochemical Pathway"
-				className="text-gray-600 text-justify"
-			>
-				<PathwayInfo />
-			</AccordionItem>
-			<AccordionItem
-				key="2"
-				title="Reaction Mechanism"
-				className="text-gray-600 text-justify"
-			>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-				sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-				Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-			</AccordionItem>
-			</Accordion>
-					<p className="text-gray-600 text-justify">
-				{assay_desc}
-					</p>
-			<Accordion isCompact variant="shadow">
-			<AccordionItem
-				key="3"
-				title="Assay Chemistry"
-				className="text-gray-600 text-justify"
-			>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-				sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-				Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-			</AccordionItem>
-			</Accordion>
+				{/* Page Title */}
+				<div className="max-w-7x1 mx-auto pt-8 mb-4">
+					<h2 className="mb-2 text-3xl md:text-4xl lg:text-5xl font-inter dark:text-white">
+						About {full_name} ({abbr})
+					</h2>
+					{coming_soon && (
+						<p>(Coming soon!)</p>
+					)}
+					{!coming_soon && (
+						<p>(Added to D2D Network in {year})</p>
+					)}
 				</div>
-			</div>
-						</div>
 
-						{/* Sequence Section */}
-						<div className="px-6 md:px-12 lg:px-24 py-16">
-				<h2 className="mb-4 text-3xl md:text-4xl font-light">Full BglB Sequence</h2>
-				
-				<p className="mb-6 text-gray-600">
-					One-letter amino acid residue codes in plain type (not bold) were not resolved in the crystal structure used for our design study 
-					(<a href="http://www.rcsb.org/structure/2JIE" className="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">PDB #2JIE</a>). 
-					One-letter codes in blue have parameter data stored in our database. Hovering over any 1-letter code in the sequence will give that residues sequence numbers/positions.
-				</p>
+				{/* Sub-Sections */}
+				<div className="max-w-7x1 mx-auto pt-8 mb-4">
 
-				<div className="font-mono text-lg leading-loose break-words">
-					{sequenceData.map((residue, index) => {
-						const hasStructure = residue.PDBresnum !== null;
-						const hasData = hasCharacterizationData(residue.Rosetta_resnum);
-
-						return (
-							<React.Fragment key={residue.id}>
-								<Tooltip
-									content={
-										<div className="text-sm">
-											{`${residue.resid}${residue.resnum}`}
-											{residue.Rosetta_resnum && (
-												<>
-													<br />
-													{`Rosetta/Foldit: ${residue.resid}${residue.Rosetta_resnum}`}
-												</>
-											)}
-											{residue.PDBresnum && (
-												<>
-													<br />
-													{`PDB: ${residue.resid}${residue.PDBresnum}`}
-												</>
-											)}
-										</div>
-									}
-								>
-									<span 
-										className={`
-											${hasData ? 'text-blue-500 cursor-pointer' : 'text-black'}
-											${hasStructure ? 'font-bold' : ''}
-										`}
-										onClick={() => {
-											if (hasData && residue.Rosetta_resnum) {
-												window.location.href = `/database/characterization_data/${enzyme}?highlight=${residue.Rosetta_resnum}`;
-											}
-										}}
-									>
-										{residue.resid}
+					{/* General Information Sub-Section: 2 Columns */}
+					<div className="inline-grid grid-cols-2 grid-rows-1 gap-4 w-full">
+						{/* Column 1*/}
+						<div>
+							{/* Idenifiers Sub-sub Section */}
+							<div className="space-y-2 mb-8">
+								<h4>Identifiers</h4>
+								<div className="flex items-center gap-3">
+									<span className="font-semibold w-44 text-sm text-gray-600">
+										Species:
 									</span>
-								</Tooltip>
-								{(index + 1) % 10 === 0 ? ' ' : ''}
-							</React.Fragment>
-						);
-					})}
-				</div>
-			</div>
+									<Link
+										href={species_link}
+										className="text-sm text-blue-500"
+										underline="hover"
+										isExternal
+										showAnchorIcon
+									>
+										<i>{species}</i>
+									</Link>
+								</div>
+								<div className="flex items-center gap-3">
+									<span className="font-semibold w-44 text-sm text-gray-600">
+										<abbr title="Enzyme Commission">EC</abbr> Number:
+									</span>
+									<Link
+										href={EC_link}
+										className="text-sm text-blue-500"
+										underline="hover"
+										isExternal
+										showAnchorIcon
+									>
+										{EC_number}
+									</Link>
+								</div>
+								<div className="flex items-center gap-3">
+									<span className="font-semibold w-44 text-sm text-gray-600">
+										<abbr title="Universal Protein Resource">UniProt</abbr> Number:
+									</span>
+									<Link
+										href={UniProt_link}
+										className="text-sm text-blue-500"
+										underline="hover"
+										isExternal
+										showAnchorIcon
+									>
+										{UniProt_number}
+									</Link>
+								</div>
+							{PDB_entries && (
+								<div className="flex items-center gap-3">
+									<span className="font-semibold w-44 text-sm text-gray-600">
+										<abbr title="Protein Data Bank">PDB</abbr> Entries:
+									</span>
+									<div className="flex flex-wrap gap-2 text-sm">
+									{pdbs.map((pdb) => (
+										<Link
+											key={pdb}
+											href={PDB_link(pdb)}
+											className="text-sm text-blue-500"
+											underline="hover"
+											isExternal
+											showAnchorIcon
+										>
+											{pdb}
+										</Link>
+									))}
+									</div>
+								</div>
+							)}
+							{AlphaFold_entries && (
+								<div className="flex items-center gap-3">
+									<span className="font-semibold w-44 text-sm text-gray-600">
+										AlphaFold Entries:
+									</span>
+									<div className="flex flex-wrap gap-2 text-sm">
+									{models.map((model) => (
+										<Link
+											key={model}
+											href={AlphaFold_link(model)}
+											className="text-sm text-blue-500"
+											underline="hover"
+											isExternal
+											showAnchorIcon
+										>
+											AF-{model}
+										</Link>
+									))}
+									</div>
+								</div>
+							)}
+							</div>
 
-			{/* Related Resources Section */}
-			<div className="px-6 md:px-12 lg:px-24 py-16">
-				<h2 className="mb-8 text-3xl md:text-4xl font-light">Related Resources</h2>
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-					{[
-						{
-							title: "Pathway and Mechanism",
-							link: "#",
-							linkText: "View Pathway and Mechanism"
-						},
-						{
-							title: "Assay Chemistry",
-							link: "#",
-							linkText: "View Assay Chemistry"
-						},
-						{
-							title: "Publications",
-							link: "/resources/publications",
-							linkText: "View Publications"
-						}
-					].map((item, index) => (
-						<Card 
-							key={index}
-							className="h-[170px] hover:scale-105 transition-transform cursor-pointer"
-							as={Link}
-							href={item.link}
-						>
-							<CardBody className="flex flex-col justify-between h-full">
-								<h3 className="text-2xl md:text-3xl lg:text-4xl font-light pl-4 pt-2">
-									{item.title}
-								</h3>
-								<span className="text-sm pl-4 pb-4 text-[#06B7DB] hover:font-semibold">
-									{item.linkText} {'>'}
-								</span>
-							</CardBody>
-						</Card>
-					))}
+							{/* Properties Sub-sub Section */}
+							<div className="space-y-2 mb-4">
+								<h4>Properties</h4>
+								<div className="flex items-center gap-3">
+									<span className="font-semibold w-44 text-sm text-gray-600">
+										Subunits (in active form):
+									</span>
+									<span className="text-sm text-gray-600">{subunits}</span>
+								</div>
+								<div className="flex items-center gap-3">
+									<span className="font-semibold w-44 text-sm text-gray-600">
+										Molar Mass (<i>M</i>, per subunit):
+									</span>
+									<span className="text-sm text-gray-600">
+										{molar_mass}{' '}
+										<abbr title="daltons (g/mol)">Da</abbr>
+									</span>
+								</div>
+								<div className="flex items-center gap-3">
+									<span className="font-semibold w-44 text-sm text-gray-600">
+										Extinction Coefficient (ε):
+									</span>
+									<span className="text-sm text-gray-600">
+										{ext_coefficient}{' '}
+										<abbr title="inverse molar per centimeter">
+											ᴍ<sup>&minus;1</sup> cm<sup>&minus;1</sup>
+										</abbr>
+									</span>
+								</div>
+							</div>
+						</div>
+
+						{/* Column 2: Image */}
+						<div>
+							<Image 
+								src={pretty_image}
+								title={pretty_image_title}
+								alt="3D Structure"
+								className="w-[280px] sm:w-[320px] lg:w-[350px] max-w-full"
+							/>
+						</div>
+					</div>
+
+					{/* Detailed Description Sub-Section 1 Column */}
+					<div className="w-full pt-8 mb-4">
+						<p className="text-gray-600 text-justify mb-4">
+							{pathway_desc}
+						</p>
+						<Accordion isCompact variant="shadow">
+							<AccordionItem
+								key="1"
+								title="Biochemical Pathway"
+								className="text-gray-600 text-justify"
+							>
+								<PathwayInfo />
+							</AccordionItem>
+							<AccordionItem
+								key="2"
+								title="Reaction Mechanism"
+								className="text-gray-600 text-justify"
+							>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+								sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							</AccordionItem>
+						</Accordion>
+						<p className="text-gray-600 text-justify mb-4">
+							{assay_desc}
+						</p>
+						<Accordion isCompact variant="shadow">
+							<AccordionItem
+								key="3"
+								title="Assay Chemistry"
+								className="text-gray-600 text-justify"
+							>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+								sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							</AccordionItem>
+						</Accordion>
+					</div>
+
+					{/* Sequence Sub-Section */}
+					<div className="w-full pt-8 mb-4">
+						<h3 className="mb-4 text-3xl md:text-4xl font-light">Full BglB Sequence</h3>
+						<p className="mb-6 text-gray-600">
+							One-letter amino acid residue codes in plain type (not bold) were not resolved in the crystal structure used for our design study 
+							(<a href="http://www.rcsb.org/structure/2JIE" className="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">PDB #2JIE</a>). 
+							One-letter codes in blue have parameter data stored in our database. Hovering over any 1-letter code in the sequence will give that residues sequence numbers/positions.
+						</p>
+
+						<div className="px-6 md:px-12 lg:px-24 py-16 font-mono text-lg leading-loose break-words">
+							{sequenceData.map((residue, index) => {
+								const hasStructure = residue.PDBresnum !== null;
+								const hasData = hasCharacterizationData(residue.Rosetta_resnum);
+
+								return (
+									<React.Fragment key={residue.id}>
+										<Tooltip
+											content={
+												<div className="text-sm">
+													{`${residue.resid}${residue.resnum}`}
+													{residue.Rosetta_resnum && (
+														<>
+															<br />
+															{`Rosetta/Foldit: ${residue.resid}${residue.Rosetta_resnum}`}
+														</>
+													)}
+													{residue.PDBresnum && (
+														<>
+															<br />
+															{`PDB: ${residue.resid}${residue.PDBresnum}`}
+														</>
+													)}
+												</div>
+											}
+										>
+											<span 
+												className={`
+													${hasData ? 'text-blue-500 cursor-pointer' : 'text-black'}
+													${hasStructure ? 'font-bold' : ''}
+												`}
+												onClick={() => {
+													if (hasData && residue.Rosetta_resnum) {
+														window.location.href = `/database/characterization_data/${enzyme}?highlight=${residue.Rosetta_resnum}`;
+													}
+												}}
+											>
+												{residue.resid}
+											</span>
+										</Tooltip>
+										{(index + 1) % 10 === 0 ? ' ' : ''}
+									</React.Fragment>
+								);
+							})}
+						</div>
+					</div>
+
+					{/* Publications Sub-Section */}
+					<div className="w-full pt-8 mb-4">
+						<h3 className="mb-4 text-3xl md:text-4xl font-light">Publications Related to {enzyme}</h3>
+						<p  className="mb-6 text-gray-600">Coming soon...</p>
+					</div>
 				</div>
 			</div>
 
