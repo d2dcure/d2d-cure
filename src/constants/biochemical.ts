bcd8099ef9103ed41d70bb8afe64ffcb15f723ae
Labonte <jlabonte@ndm.edu>
2026-07-25 16:56:14 -0400

    classifying AAs

--- src/constants/biochemical.ts
+++ src/constants/biochemical.ts
@@ -3,9 +3,27 @@
 // This file contains biochemical constants and data that can be imported for
 // use in the D2D database website.
 
-// Imports
 
 export const canonicalAAs = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
 		'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
 
+export const negativeAAs = ['D', 'E']
 
+export const positiveAAs = ['H', 'K', 'R']
+
+export const polarChargedAAs = negativeAAs.concat(positiveAAs)
+
+export const polarUnhargedAAs = ['C', 'N', 'Q', 'S', 'T', 'Y']
+
+export const hydrophilicAAs = polarChargedAAs.concat(polarUnhargedAAs)
+
+export const hydrophobicAAs =
+		canonicalAAs.filter((aa) => !hydrophilicAAs.includes(aa))
+
+export const aromaticAAs = ['H', 'F', 'Y', 'W']
+
+export const helixBreakingAAs = ['G', 'P']
+
+export const smallAAs = ['A', 'G']
+
+export const bulkyAAs = ['E', 'F', 'H', 'I', 'K', 'L', 'M', 'Q', 'R', 'Y', 'W']
