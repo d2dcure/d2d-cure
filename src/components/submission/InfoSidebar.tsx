082126c460685d34857da92f6e4d0556770606b8
Labonte <jlabonte@ndm.edu>
2026-07-27 04:25:04 -0400

    no, I forgot that this is for the WT

--- src/components/submission/InfoSidebar.tsx
+++ src/components/submission/InfoSidebar.tsx
@@ -259,7 +259,7 @@ const SingleVarSidebar: React.FC<SidebarProps> = (
 				<Link
 					isExternal
 					showAnchorIcon
-					href={`/database/characterization_data/${enzyme}?highlight=${entryData.Rosetta_resnum}`}
+					href={`/database/characterization_data/${enzyme}?highlight=${entryData.resnum}`}
 				>
 					Search database
 				</Link>
